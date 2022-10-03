a10c132b8f647479ec02320639c2a8e5cbef60b3
Pheenoh <pheenoh@gmail.com>
2022-10-02 22:06:52 -0600

    tmp fix

--- main.tf
+++ main.tf
@@ -1,31 +1,33 @@
 module "aws" {
-  count                   = lower(var.platform) == "aws" ? 1 : 0
-  source                  = "./modules/aws"
-  region                  = var.aws_region
-  cidr_block              = var.aws_cidr_block
-  create_dns_record       = var.aws_create_dns_record
-  create_networking       = var.aws_create_networking
-  dns_record_name         = var.aws_dns_record_name
-  dns_record_ttl          = var.aws_dns_record_ttl
-  dns_zone_name           = var.aws_dns_zone_name
-  ghidra_install_path     = var.ghidra_install_path
-  ghidra_name             = var.ghidra_name
-  ghidra_server_config    = var.ghidra_server_config
-  ghidra_server_log_level = var.ghidra_server_log_level
-  ghidra_repo_path        = var.ghidra_repo_path
-  ghidra_uri_override     = var.ghidra_uri_override
-  ghidra_uri              = var.ghidra_uri
-  ghidra_version          = var.ghidra_version
-  ghidra_version_map      = var.ghidra_version_map
-  initial_java_heap_size  = var.initial_java_heap_size
-  instance_type           = var.aws_instance_type
-  max_java_heap_size      = var.max_java_heap_size
-  repo_volume_size        = var.repo_volume_size
-  repo_volume_type        = var.aws_volume_type
-  s3_backup               = var.aws_s3_backup
-  s3_bucket_name          = var.aws_s3_bucket_name
-  subnet_id               = var.aws_subnet_id
-  vpc_id                  = var.aws_vpc_id
+  count                      = lower(var.platform) == "aws" ? 1 : 0
+  source                     = "./modules/aws"
+  region                     = var.aws_region
+  cidr_block                 = var.aws_cidr_block
+  create_dns_record          = var.aws_create_dns_record
+  create_networking          = var.aws_create_networking
+  dns_record_name            = var.aws_dns_record_name
+  dns_record_ttl             = var.aws_dns_record_ttl
+  dns_zone_name              = var.aws_dns_zone_name
+  ghidra_install_path        = var.ghidra_install_path
+  ghidra_name                = var.ghidra_name
+  ghidra_server_config       = var.ghidra_server_config
+  ghidra_server_log_level    = var.ghidra_server_log_level
+  ghidra_repo_path           = var.ghidra_repo_path
+  ghidra_uri_override        = var.ghidra_uri_override
+  ghidra_uri                 = var.ghidra_uri
+  ghidra_version             = var.ghidra_version
+  ghidra_version_map         = var.ghidra_version_map
+  ghidra_zip_file_name       = var.ghidra_zip_file_name
+  ghidra_extract_folder_name = var.ghidra_extract_folder_name
+  initial_java_heap_size     = var.initial_java_heap_size
+  instance_type              = var.aws_instance_type
+  max_java_heap_size         = var.max_java_heap_size
+  repo_volume_size           = var.repo_volume_size
+  repo_volume_type           = var.aws_volume_type
+  s3_backup                  = var.aws_s3_backup
+  s3_bucket_name             = var.aws_s3_bucket_name
+  subnet_id                  = var.aws_subnet_id
+  vpc_id                     = var.aws_vpc_id
 }
 
 module "linode" {
